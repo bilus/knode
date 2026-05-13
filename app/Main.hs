69c1901f8adc21427668495f354056da58c4a56d
Marcin Bilski <gyamtso@gmail.com>
2026-05-14 00:55:18 +0200

    Prepare repo.

--- app/Main.hs
+++ app/Main.hs
@@ -7,15 +7,21 @@ module Main (main) where
 -- import Lib
 
 import Control.Monad (when)
-import Control.Monad.Except (ExceptT (..), runExceptT, throwError)
+import Control.Monad.Except (ExceptT (..), MonadError (catchError), runExceptT, throwError)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans (lift)
 import qualified Data.ByteString as BS
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
-import Shelly (Sh)
+import Shelly (Sh, whenM)
 import qualified Shelly as Sh
 import System.FilePath ((</>))
+
+type App a = ExceptT AppError Sh a
+
+runApp :: App a -> IO (Either AppError a)
+runApp = Sh.shelly . (Sh.errExit False) . runExceptT
 
 data Page = Page FilePath
 
@@ -25,24 +31,24 @@ data Change
     | Edit !Page !Text !Text
 
 -- | Remote git repository url.
-data RepoURL = RepoURL Text
+data RepoUrl = RepoUrl Text
     deriving (Show)
 
 -- | Information about a locally cloned git repository.
 data ClonedRepo = ClonedRepo
     { repoPath :: !FilePath
-    , repoUrl :: !RepoURL
+    , repoUrl :: !RepoUrl
     }
     deriving (Show)
 
 -- | Apply a change to a page in the repository.
-applyChange :: ClonedRepo -> Change -> Sh ()
+applyChange :: ClonedRepo -> Change -> App ()
 applyChange repo (Write page content) = write repo page content
 applyChange repo (Edit page oldContent newContent) = edit repo page oldContent newContent
 
 -- | Stage and commit a change to the repository.
-commit :: ClonedRepo -> Change -> Sh ()
-commit repo@(ClonedRepo{..}) change = do
+commit :: ClonedRepo -> Change -> App ()
+commit repo@(ClonedRepo{..}) change = lift $ do
     Sh.cd repoPath
     Sh.run_ "git" ["add", T.pack pagePath]
     Sh.run_ "git" ["commit", "-m", commitMsg]
@@ -52,73 +58,116 @@ commit repo@(ClonedRepo{..}) change = do
         Edit page _ _ -> (page, "Edited " <> T.pack (pageFullPath repo page))
 
 -- | Write content to a page, overwriting any existing content.
-write :: ClonedRepo -> Page -> Text -> Sh ()
+write :: ClonedRepo -> Page -> Text -> App ()
 write repo page content = do
-    Sh.liftIO $ BS.writeFile (pageFullPath repo page) (TE.encodeUtf8 content)
+    liftIO $ BS.writeFile (pageFullPath repo page) (TE.encodeUtf8 content)
 
 -- | Replace old content with new content in a page.
-edit :: ClonedRepo -> Page -> Text -> Text -> Sh ()
+edit :: ClonedRepo -> Page -> Text -> Text -> App ()
 edit = undefined
 
-data ShellError = ShellError {exitCode :: Int}
+data AppError
+    = ShellError {exitCode :: Int}
+    | WrongRepo RepoUrl FilePath
+    | NoSuchDir FilePath
     deriving (Show)
 
+-- | Lift a Sh action into App, converting exceptions to the given error.
+liftSh :: AppError -> Sh a -> App a
+liftSh err action =
+    ExceptT $
+        Sh.catchany_sh
+            (Right <$> action)
+            (const $ return $ Left err)
+
 -- | Run a shell command, returning stdout or an error with exit code.
-shell :: (Maybe FilePath) -> String -> [Text] -> Sh (Either ShellError Text)
-shell cwd cmd args = Sh.errExit False $ do
+shell :: (Maybe FilePath) -> String -> [Text] -> App Text
+shell cwd cmd args = do
     case cwd of
-        Just path -> Sh.cd path -- TODO: Catch exception.
+        Just path -> liftSh (NoSuchDir path) $ Sh.cd path
         Nothing -> return ()
-    output <- Sh.run cmd args
-    exitCode <- Sh.lastExitCode
-    return $
-        if exitCode /= 0
-            then Left (ShellError exitCode)
-            else
-                Right output
+    output <- lift $ Sh.run cmd args
+    exitCode <- lift $ Sh.lastExitCode
+    when (exitCode /= 0) $ throwError $ ShellError exitCode
+    return output
 
 -- | Run a shell command, discarding stdout.
-shell_ :: (Maybe FilePath) -> String -> [Text] -> Sh (Either ShellError ())
-shell_ cwd cmd args = fmap (() <$) $ shell cwd cmd args
+shell_ :: (Maybe FilePath) -> String -> [Text] -> App ()
+shell_ cwd cmd args = do
+    _ <- shell cwd cmd args
+    return ()
 
 -- | Clone a git repository to the specified path.
-cloneRepo :: RepoURL -> FilePath -> Sh (Either ShellError ClonedRepo)
-cloneRepo repoUrl@(RepoURL repo) repoPath = do
-    result <- shell_ Nothing "git" ["clone", repo, T.pack repoPath]
-    return $ (const ClonedRepo{..}) <$> result
+cloneRepo :: RepoUrl -> FilePath -> App ClonedRepo
+cloneRepo repoUrl@(RepoUrl repo) repoPath = do
+    shell_ Nothing "git" ["clone", repo, T.pack repoPath]
+    return ClonedRepo{..}
+
+-- | True if action successful.
+success :: App a -> App Bool
+success m = catchError (True <$ m) (const $ return False)
+
+-- | Is the directory a git repo.
+isGitRepo :: FilePath -> App Bool
+isGitRepo repoPath = do
+    exists <- lift $ Sh.test_d repoPath
+    if (not exists)
+        then
+            return False
+        else
+            success $ shell (Just repoPath) "git" ["rev-parse", "--git-dir"]
+
+-- | Is the directory this git repo. Error if not git repo or dir doesn't exist.
+isThisRepo :: RepoUrl -> FilePath -> App Bool
+isThisRepo (RepoUrl repoUrl) repoPath = do
+    url <- shell (Just repoPath) "git" ["remote", "get-url", "origin"]
+    return $ T.strip url == repoUrl
 
 -- | Fetch and reset an existing repository to match origin/HEAD.
-rebaseRepo :: RepoURL -> FilePath -> Sh (Either ShellError ClonedRepo)
-rebaseRepo repoUrl repoPath = runExceptT $ do
-    exists <- lift $ Sh.test_d repoPath
-    when (not exists) $ throwError $ ShellError{exitCode = 1} -- TODO: Sum type to give an error message
-    _ <- ExceptT $ shell (Just repoPath) "git" ["fetch", "origina"]
-    _ <- ExceptT $ shell (Just repoPath) "git" ["reset", "--hard", "origin/HEAD"]
+resetRepo :: RepoUrl -> FilePath -> App ClonedRepo
+resetRepo repoUrl repoPath = do
+    whenM
+        (not <$> (isThisRepo repoUrl repoPath))
+        (throwError $ WrongRepo repoUrl repoPath)
+
+    shell_ (Just repoPath) "git" ["fetch", "origin"]
+    shell_ (Just repoPath) "git" ["reset", "--hard", "origin/HEAD"]
     return ClonedRepo{..}
 
 -- | Clone a repository if it doesn't exist, or update it if it does.
-ensureRepoCloned :: RepoURL -> FilePath -> IO (Either Text ClonedRepo)
-ensureRepoCloned repoUrl@(RepoURL repo) repoPath = Sh.shelly $ do
-    -- TODO: Check if the repo already exists and is fresh.
-    Sh.run_ "git" ["clone", repo, T.pack repoPath]
-    return $ Right ClonedRepo{..}
+prepareRepo :: RepoUrl -> FilePath -> App ClonedRepo
+prepareRepo repoUrl repoPath =
+    ifM
+        (isGitRepo repoPath)
+        (resetRepo repoUrl repoPath)
+        (cloneRepo repoUrl repoPath)
 
 -- | Get the full filesystem path for a page within a repository.
 pageFullPath :: ClonedRepo -> Page -> FilePath
 pageFullPath (ClonedRepo{repoPath}) (Page pagePath) =
     repoPath </> pagePath
 
+ifM :: (Monad m) => m Bool -> m a -> m a -> m a
+ifM cond t f = cond >>= \b -> if b then t else f
+
 -- | Entry point.
 main :: IO ()
-main = Sh.shelly $ do
-    output <- shell (Just "/tmp/repo") "git" ["status"]
-    write (ClonedRepo{repoPath = ".", repoUrl = RepoURL ""}) (Page "test.txt") "Hello, World!"
-    Sh.liftIO $ print output
+main = do
+    result <- runApp $ do
+        repo <- prepareRepo repoUrl repoPath
+        liftIO $ putStrLn (show repo)
+
+    case result of
+        Right _ -> putStrLn "Success!"
+        Left e -> putStrLn $ show e
+  where
+    repoPath = "/tmp/repo"
+    repoUrl = sampleRepoUrl
 
 ---------
 -- Sample data for testing
 
-sampleRepoUrl = RepoURL "git@github.com:bilus/fencer.git"
+sampleRepoUrl = RepoUrl "git@github.com:bilus/fencer.git"
 
 sampleRepo =
     ClonedRepo
